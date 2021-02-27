45e399f049f307e636f55cdd879ed90b32f26cf1
EgorPekhota <pehota.egor@gmail.com>
2021-02-27 13:20:47 +0300

    Initial Commit

---
+++ HarvardCourse_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HarvardCourse_01
+//
+//  Created by Egor Pekhota on 27.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
